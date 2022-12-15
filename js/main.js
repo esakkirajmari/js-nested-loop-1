d0a12d1f2f0227b36ed2c8107b7de0afb8eb1809
esakkirajmari <esakkirajmari@gmail.com>
2022-12-15 09:29:55 +0530

    js/

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-let num=parseInt(prompt("enter the number"));
-for (let i=num; i>0; i--){
-    for(let j=1; j<=i; j++){
+let i,j;
+for(i=5;i>=1;i--){
+    for(j=1;j<=i;j++){
         document.write(j)
     }
     document.write("<br>")
